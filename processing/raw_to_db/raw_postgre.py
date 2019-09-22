c1e714c16ff143508d811ef4968883d75f1fc4c2
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2019-09-22 23:39:27 +0000

    spark to postgreSql

--- processing/raw_to_db/raw_postgre.py
+++ processing/raw_to_db/raw_postgre.py
@@ -1,11 +1,32 @@
 import subprocess
-#import pyspark
+from pyspark import SparkContext, SparkConf
 import boto3
 import botocore
 import pandas as pd
 import io
 import psycopg2
+from operator import add
+from mySecret import db_host, db, db_user, db_password, table
 
+#def get_all_s3_keys(bucket_name, dir):
+def get_all_s3_keys(bucket_name):
+    """Get a list of all keys in an S3 bucket."""
+    clinet = boto3.client('s3')
+    keys = []
+
+    kwargs = {'Bucket': bucket_name}
+    while True:
+        #resp = clinet.list_objects_v2(Bucket=bucket_name,Prefix=dir)
+        resp = clinet.list_objects_v2(Bucket=bucket_name)
+        for obj in resp['Contents']:
+            keys.append(obj['Key'])
+
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
+
+    return keys
 
 #write the contact map to the database
 def send_CM_to_DB(str_id, pro_contains_resArry, csr, tb):
@@ -15,33 +36,37 @@ def send_CM_to_DB(str_id, pro_contains_resArry, csr, tb):
     csr.execute(cmd, (data))
 
 def main():
-    table = 'structures'
-    connection = psycopg2.connect(host = ip, database = db, user = usr, password = pw)
+    connection = psycopg2.connect(host = db_host, database = db, user = db_user, password = db_password)
     cursor = connection.cursor()
-
-    #data processing
-    bucket_name = 'protein-structures'
-    dir = 'test_data/test_structures/T0949/'
+    #bucket_name = 'protein-structures'
+    #dir = 'test_data/test_structures/T0949/'
+    bucket_name = 'namehard'
     
-    clinet = boto3.client('s3')
+    conf = SparkConf().setMaster("spark://ip-10-0-0-12:7077").setAppName("s3ToLIC")
+    sc = SparkContext(conf=conf)
+    #sc = SparkContext(conf=conf).setLogLevel("ERROR")
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
-    i = 0
-    resp = clinet.list_objects_v2(Bucket=bucket_name,Prefix=dir)
+    #keys = get_all_s3_keys(bucket_name, dir)
+    keys = get_all_s3_keys(bucket_name)
+    pkeys = sc.parallelize(keys)
+    #i = 0
+    #sc.parallelize(i)
+    print(pkeys.take(10))
 
-    for obj in resp['Contents']:
-        print(obj['Key'])
+    for obj in pkeys.toLocalIterator():
+        #print(obj)
         '''
         if i > 0:
             break;
         i = i + 1;
         '''
-        args = ("./LIC", bucket_name, obj['Key'])
+        args = ("./LIC", bucket_name, obj)
         popen = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = popen.stdout.read()
         pro_contains_resArry = output.decode()
         #for each structure, save all its residue-residue contact map to database
-        send_CM_to_DB(obj['Key'], pro_contains_resArry, cursor, table)
+        send_CM_to_DB(obj, pro_contains_resArry, cursor, table)
 
     connection.commit()
     #to read the first row in the contact map
