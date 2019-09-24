e433f79c2d56b54d12fef3cef94cd768ef01a582
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2019-09-24 13:48:37 +0000

    not using spark workers. Everything else runs well

--- processing/raw_to_db/raw_postgre.py
+++ processing/raw_to_db/raw_postgre.py
@@ -6,17 +6,28 @@ import pandas as pd
 import io
 import psycopg2
 from operator import add
-from mySecret import db_host, db, db_user, db_password, table
+from mySecret import db_host, db, db_user, db_password, AWS_ACCESS_KEY, AWS_SECRET_KEY
+import smart_open
 
-#def get_all_s3_keys(bucket_name, dir):
+def get_a_fold_keys(bucket_name, dir):
+    """List files in specific S3 URL"""
+    client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY,\
+                      aws_secret_access_key=AWS_SECRET_KEY)
+    keys = []
+    response = client.list_objects(Bucket=bucket_name, Prefix=dir)
+    for content in response.get('Contents', []):
+        keys.append(content.get('Key'))
+    return keys
+
+
 def get_all_s3_keys(bucket_name):
     """Get a list of all keys in an S3 bucket."""
-    clinet = boto3.client('s3')
+    clinet = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY,\
+                      aws_secret_access_key=AWS_SECRET_KEY)
     keys = []
 
-    kwargs = {'Bucket': bucket_name}
+    kwargs = {'Bucket': bucket_name, 'Prefix': dir}
     while True:
-        #resp = clinet.list_objects_v2(Bucket=bucket_name,Prefix=dir)
         resp = clinet.list_objects_v2(Bucket=bucket_name)
         for obj in resp['Contents']:
             keys.append(obj['Key'])
@@ -29,44 +40,72 @@ def get_all_s3_keys(bucket_name):
     return keys
 
 #write the contact map to the database
-def send_CM_to_DB(str_id, pro_contains_resArry, csr, tb):
+def send_CM_to_DB(str_id, pro_contains_resArry, csr):
     pro_contains_resArry = pro_contains_resArry[:-1].split('\n')
     cmd = """INSERT INTO structures VALUES (%s, %s)"""
     data = (str_id, (pro_contains_resArry))
     csr.execute(cmd, (data))
 
+
+
+def send_score_to_DB(str_id, score, csr):
+    cmd = """UPDATE structures SET score = %s WHERE structure_id = %s """
+    data = (score, str_id)
+    #print(score)
+    #print(str_id)
+    csr.execute(cmd, data)
+
+
 def main():
     connection = psycopg2.connect(host = db_host, database = db, user = db_user, password = db_password)
     cursor = connection.cursor()
-    #bucket_name = 'protein-structures'
-    #dir = 'test_data/test_structures/T0949/'
-    bucket_name = 'namehard'
-    
+    bucket_name = 'protein-structures'
+    struct_dir = 'test_data/test_structures/T0950/'
+    score_dir = 'test_data/test_scores/T0950/'
+    #table = 'structures'
+
     conf = SparkConf().setMaster("spark://ip-10-0-0-12:7077").setAppName("s3ToLIC")
     sc = SparkContext(conf=conf)
     #sc = SparkContext(conf=conf).setLogLevel("ERROR")
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-    #keys = get_all_s3_keys(bucket_name, dir)
-    keys = get_all_s3_keys(bucket_name)
-    pkeys = sc.parallelize(keys)
-    #i = 0
-    #sc.parallelize(i)
-    print(pkeys.take(10))
+    #keys = get_all_s3_keys(bucket_name)
+    structs = get_a_fold_keys(bucket_name, struct_dir)
+    #print(type(structs))
+    pstructs = sc.parallelize(structs)
+    scores = get_a_fold_keys(bucket_name, score_dir)
+    pscores = sc.parallelize(scores)
+    #print(pscores.take(5)) 
 
-    for obj in pkeys.toLocalIterator():
-        #print(obj)
-        '''
-        if i > 0:
-            break;
-        i = i + 1;
-        '''
+    for obj in pstructs.toLocalIterator():
         args = ("./LIC", bucket_name, obj)
         popen = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = popen.stdout.read()
         pro_contains_resArry = output.decode()
         #for each structure, save all its residue-residue contact map to database
-        send_CM_to_DB(obj, pro_contains_resArry, cursor, table)
+        obj = obj.strip().split('/')
+        #for real data
+        #str_id = obj[-3] + "/" + obj[-1]
+        #for debug data
+        str_id = obj[-1]
+        send_CM_to_DB(str_id, pro_contains_resArry, cursor)
+        #break
+
+
+    for obj in pscores.toLocalIterator():
+        #print(obj)
+        flag = False
+        for line in smart_open.open('s3://'+bucket_name+'/'+obj):
+            if (line.startswith("SUMMARY(GDT)")):
+                line = line.strip().split()
+                obj = obj.strip().split('/')
+                #for real data
+                str_id = obj[-3] + "/" + obj[-1]
+                #for debug data
+                str_id = obj[-1].split('.')
+                str_id = str_id[-2]
+                send_score_to_DB(str_id, float(line[6]), cursor)
+                break
+
+
 
     connection.commit()
     #to read the first row in the contact map
@@ -74,6 +113,11 @@ def main():
     #bad idea because of memory issue: print(cursor.fetchall())
     cursor.close()
     connection.close()
+
+    #s3 = boto3.resource('s3')
+    #bucket = s3.Bucket(bucket_name)
+
+
 
 if __name__ == "__main__":
     main()
---
+++ processing/raw_to_db/str_eval_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE structures(
+	structure_id TEXT PRIMARY KEY,
+        contact_map  TEXT[],
+        score        REAL
+);
