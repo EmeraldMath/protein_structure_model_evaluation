d9535b62d902d7984b944128c370ca2173fabcf0
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2019-10-15 18:00:39 +0000

    orginize

--- DE/etl/candidate.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE candidates(
-        candidate_id      TEXT PRIMARY KEY,
-  protein_id        TEXT,
-  contact_map       BYTEA,
-  candidate_source  TEXT,
-  score             REAL DEFAULT -1.0,
-);
--- DS/cnn/prediction.sql
+++
@@ -1,4 +0,0 @@
-REATE TABLE prediction(
-        candidate_id      TEXT PRIMARY KEY,
-  label             INT
-);
